66814d1e4a189d53fc5c638c0d0146c0102cda31
sidhesh77 <www.sidhesh@gmail.com>
2026-02-05 14:00:00 +0530

    remove target/

--- crates/http-server/target/debug/deps/http_server-8b9450d415487a5c.d
+++
@@ -1,5 +0,0 @@
-/Users/shivraj/Developer/cool_proj/fake-email/crates/http-server/target/debug/deps/http_server-8b9450d415487a5c.d: src/main.rs
-
-/Users/shivraj/Developer/cool_proj/fake-email/crates/http-server/target/debug/deps/libhttp_server-8b9450d415487a5c.rmeta: src/main.rs
-
-src/main.rs:
--- crates/http-server/target/debug/deps/http_server-e69230bb9180c5b5.d
+++
@@ -1,5 +0,0 @@
-/Users/shivraj/Developer/cool_proj/fake-email/crates/http-server/target/debug/deps/http_server-e69230bb9180c5b5.d: src/main.rs
-
-/Users/shivraj/Developer/cool_proj/fake-email/crates/http-server/target/debug/deps/libhttp_server-e69230bb9180c5b5.rmeta: src/main.rs
-
-src/main.rs:
