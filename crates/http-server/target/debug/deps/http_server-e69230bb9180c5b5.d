9532eae4595b72ad8e8ac8c9ebdab673597cf3bd
sidhesh77 <www.sidhesh@gmail.com>
2026-02-05 10:00:00 +0530

    http-server init

---
+++ crates/http-server/target/debug/deps/http_server-e69230bb9180c5b5.d
@@ -0,0 +1,5 @@
+/Users/shivraj/Developer/cool_proj/fake-email/crates/http-server/target/debug/deps/http_server-e69230bb9180c5b5.d: src/main.rs
+
+/Users/shivraj/Developer/cool_proj/fake-email/crates/http-server/target/debug/deps/libhttp_server-e69230bb9180c5b5.rmeta: src/main.rs
+
+src/main.rs:
